4848eac2d964e0005d6c5e12598c4ee4e1995333
labstraction <45163263+labstraction@users.noreply.github.com>
2022-04-06 13:30:45 +0200

    quello

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
+// setTimeout(() => {
+//     console.log("pippo")
+// }, 10000);
+
+// let intervalCounter = 0;
+// const interval = setInterval(() => {
+//     console.log(intervalCounter);
+//     if (intervalCounter === 10) {
+//         clearInterval(interval);
+//     }
+//     intervalCounter++
+// }, 1000);
+
+
 const button3 = document.getElementById('button3')
 
 button3.onclick = () => buttonClick('button 3');
@@ -10,7 +24,7 @@ const button4 = document.getElementById('button4')
 button4.addEventListener('mousedown',buttonClick)
 
 
-document.addEventListener('keydown', changeBackground);
+document.addEventListener('keydown', realTimeSearch);
 
 
 
@@ -22,6 +36,15 @@ document.addEventListener('keydown', changeBackground);
 const link = document.getElementById('link');
 
 link.addEventListener('click', displayPar)
+
+
+const div = document.getElementById('mouse-div-2');
+
+// div.addEventListener('mouseenter', onMouseEnter);
+
+// div.addEventListener('mouseleave', onMouseLeave)
+
+div.addEventListener('mousemove', onMouseOver);
 
 function buttonClick(string) {
     console.log(string);
@@ -54,4 +77,38 @@ function changeBackground(event){
         event.preventDefault(); 
         document.body.style.backgroundColor = '#'+number+number+number+number+number+number
     }
+}
+
+
+
+function onMouseEnter() {
+    console.log("sono entrato");
+}
+
+function onMouseLeave() {
+    console.log("sono uscito");
+}
+
+
+function onMouseOver() {
+    console.log("sono sopra");
+}
+
+
+let selectedKey = "";
+function onKeyDown(event) {
+    if (event.key !== selectedKey) {
+        console.log("tasto premuto " + event.key);
+        1
+    }
+}
+
+let searchTimeout;
+function realTimeSearch(event){
+    if (searchTimeout) {
+        clearTimeout(searchTimeout);
+    }
+    searchTimeout = setTimeout(() => {
+        console.log("sto cercando")
+    }, 1000);
 }
